46dd2eb115efa4fcc24dc833ad81b76960b9c39b
NoHomey <ivo.stratev.tues@gmail.com>
2017-12-06 00:54:04 +0200

    adding single thread merge sort

--- src/test.cc
+++ src/test.cc
@@ -23,11 +23,19 @@ void print(const StudentExams& exams) {
     }
 }
 
+bool lessThanOrEqualAge(const StudentExam& a, const StudentExam& b) noexcept {
+    return a.getAge() <= b.getAge();
+}
+
+bool greaterThanOrEqualAge(const StudentExam& a, const StudentExam& b) noexcept {
+    return a.getAge() >= b.getAge();
+}
+
 int main() {
     const ExamDate examDate{2, 12, 2017};
-    StudentExams exams{100};
+    StudentExams exams{10};
 
-    exams.append({examDate, 21, 6});
+    exams.append({examDate, 20, 6});
     exams.append({examDate, 21, 5});
     exams.append({examDate, 21, 3});
     exams.append({examDate, 21, 4});
@@ -44,13 +52,13 @@ int main() {
 
     print(exams);
 
-    exams.bucketSort<5>(gradeSelector);
+    exams.mergeSort(lessThanOrEqualAge);
 
     std::cout << "---------------------------------------------" << std::endl;
 
     print(exams);
 
-    exams.bucketSort<5>(gradeSelector, false);
+    exams.mergeSort(greaterThanOrEqualAge);
 
     std::cout << "---------------------------------------------" << std::endl;
 
