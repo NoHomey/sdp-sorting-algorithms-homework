28fe26bf519db9aa75cb77725b72c62afd2b9ff4
NoHomey <ivo.stratev.tues@gmail.com>
2017-12-08 18:08:47 +0200

    reducing SingleLinkedList memory footprint

--- src/test.cc
+++ src/test.cc
@@ -35,20 +35,20 @@ int main() {
     const ExamDate examDate{2, 12, 2017};
     StudentExams exams{10};
 
-    exams.append({examDate, 20, 6});
-    exams.append({examDate, 21, 5});
-    exams.append({examDate, 21, 3});
-    exams.append({examDate, 21, 4});
-    exams.append({examDate, 21, 4});
-    exams.append({examDate, 21, 5});
-    exams.append({examDate, 22, 6});
-    exams.append({examDate, 20, 6});
-    exams.append({examDate, 20, 6});
-    exams.append({examDate, 20, 2});
-    exams.append({examDate, 20, 3});
-    exams.append({examDate, 19, 6});
-    exams.append({examDate, 19, 2});
-    exams.append({examDate, 19, 2});
+    exams.prepend({examDate, 20, 6});
+    exams.prepend({examDate, 21, 5});
+    exams.prepend({examDate, 21, 3});
+    exams.prepend({examDate, 21, 4});
+    exams.prepend({examDate, 21, 4});
+    exams.prepend({examDate, 21, 5});
+    exams.prepend({examDate, 22, 6});
+    exams.prepend({examDate, 20, 6});
+    exams.prepend({examDate, 20, 6});
+    exams.prepend({examDate, 20, 2});
+    exams.prepend({examDate, 20, 3});
+    exams.prepend({examDate, 19, 6});
+    exams.prepend({examDate, 19, 2});
+    exams.prepend({examDate, 19, 2});
 
     print(exams);
 
@@ -59,6 +59,18 @@ int main() {
     print(exams);
 
     exams.mergeSort(greaterThanOrEqualAge);
+
+    std::cout << "---------------------------------------------" << std::endl;
+    
+    print(exams);
+
+    exams.bucketSort<5>(gradeSelector);
+
+    std::cout << "---------------------------------------------" << std::endl;
+
+    print(exams);
+
+    exams.bucketSort<5>(gradeSelector, false);
 
     std::cout << "---------------------------------------------" << std::endl;
 
