0fe7833da1cd07bee705a08f778fae7d873be247
NoHomey <ivo.stratev.tues@gmail.com>
2017-12-09 16:35:37 +0200

    adding desc order iterator which simulates sorting strings in desc order with minimum memory footprint

--- src/trieTest.cc
+++ src/trieTest.cc
@@ -24,27 +24,27 @@ int main() {
     SparseTrie<StudentExam> trie;
 
     trie.insert("abcd", {examDate, 21, 6});
-
     trie.insert("abbb", {examDate, 19, 6});
-
     trie.insert("bbb", {examDate, 19, 6});
-
     trie.insert("a", {examDate, 20, 5});
-
     trie.insert("b", {examDate, 21, 5});
-
     trie.insert("aabb", {examDate, 19, 4});
-
     trie.insert("abcde", {examDate, 19, 3});
-
+    trie.insert("abcdee", {examDate, 19, 3});
     trie.insert("abcdf", {examDate, 20, 5});
+    trie.insert("bb", {examDate, 19, 6});
+    trie.insert("ab", {examDate, 20, 5});
+    trie.insert("abc", {examDate, 21, 5});
+    trie.insert("abb", {examDate, 19, 4});
+    trie.insert("aa", {examDate, 19, 3});
+    trie.insert("aab", {examDate, 20, 5});
 
     std::cout << trie.size() << std::endl;
 
-    SparseTrie<StudentExam>::AscOrderConstIterator iter = trie.ascOrderFirst();
+    SparseTrie<StudentExam>::DescOrderConstIterator iter = trie.descOrderFirst();
     for(std::size_t i = 0; iter; ++iter, ++i) {
         auto res = *iter;
-        std::cout << i << ' ' << res.first << ' ' << res.second << std::endl;
+        std::cout << i << ' ' << res.first << ' ' << res.second;
     }
 
     return 0;
